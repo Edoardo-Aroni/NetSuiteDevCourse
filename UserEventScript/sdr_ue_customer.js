fc4a7f6322066f5045dde880446bd6a55d8df308
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-08-21 21:20:14 +0100

    submit

--- UserEventScript/sdr_ue_customer.js
+++ UserEventScript/sdr_ue_customer.js
@@ -2,10 +2,10 @@
  * @NScriptType UserEventScript
  * @NApiVersion 2.0
  */
-
+Define([], function(){
 return {
     afterSubmit: function(context){
         log.debug ('hello world');
         
     }
-};
+}});
