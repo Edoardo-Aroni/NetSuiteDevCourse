ca4c998aa5b9836f8a5e6734ff4e1775a2f98d53
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-08-23 17:57:20 +0100

    save

--- ClientScript/sdr_cs_customer.js
+++ ClientScript/sdr_cs_customer.js
@@ -6,17 +6,29 @@
 define([], function() {
     function fieldChanged(context){
     var customer = context.currentRecord;
-    if(context.fieldId == 'custentity_sdr_apply_coupon') {
-        var applyCoupon = customer.getValue('custentity_sdr_apply_coupon');
-        var couponCode = customer.getField('custentity_sdr_coupon_code');
-        if(applyCoupon){
-            couponCode.displyType = 'normal';
-        } else{
-            couponCode.displyType = 'disabled';
+        if(context.fieldId == 'custentity_sdr_apply_coupon') {
+            var applyCoupon = customer.getValue('custentity_sdr_apply_coupon');
+            var couponCode = customer.getField({fieldId: 'custentity_sdr_coupon_code'});
+            if(applyCoupon){
+                couponCode.isDisabled = false;
+            } else{
+                customer.setValue('custentity_sdr_coupon_code','');
+                couponCode.isDisabled = true;
+            }
         }
     }
+    function saveRecord(context){
+        var customer = context.currentRecord;
+        var applyCoupon = customer.getValue('custentity_sdr_apply_coupon');
+        var couponCode = customer.getValue('custentity_sdr_coupon_code');
+        if(applyCoupon && couponCode.length != 5 ){
+            alert('The coupon code length is not 5 characters. Please try again.');
+            return false;
+        }
+        return true;
     }
     return {
-        fieldChanged : fieldChanged
+        fieldChanged: fieldChanged,
+        saveRecord:   saveRecord
     };
 });
