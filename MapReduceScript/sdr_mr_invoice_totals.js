d60b5d5c4f586e0c9327ec6d1da4b28d28bad86a
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-08-31 18:08:12 +0100

    s

---
+++ MapReduceScript/sdr_mr_invoice_totals.js
@@ -0,0 +1,66 @@
+/**
+ * @NApiVersion 2.0,
+ * @NScriptType MapReduceScript
+ */
+
+define(['N/search'], 
+    
+    function(search){
+
+        function getInputData(){
+            var invSearch = search.create({
+                type: search.Type.TRANSACTION,
+                filters: [
+                    ['type', search.Operator.ANYOF, 'CustInvc'],
+                    'and',
+                    ['mainline', search.Operator.IS, true]
+                ],
+                columns: ['entity', 'total' ]
+            });
+
+            return invSearch;
+
+        function map(context){
+            var searchResult = JSON.parse(context.value);
+            var customer = searchResult.values.entity.text;
+            var total = searchResult.values.total;
+
+            context.write({
+                key: customer,
+                value: total
+            });
+        }
+
+        function reduce(context){
+            var total = 0;
+
+            for(var i in context.values){
+                total += parseFloat(context.values[i]);
+            }
+
+            log.debug('Total', 'Customer ' + context.key + '\n' +
+                               'Total : ' + total);
+
+        }
+
+        function summarize(summary){
+            log.audit('Number of queues', summary.concurrency);
+
+            log.error('Input error', summary.inputSummary.error);
+
+            summary.reduceSummary.errors.iterators().each(function(code, message){
+                log.error('Reduce Error: ' + code, message);
+                return true;
+            });
+        }
+
+
+        }
+        return{
+            getInputData: getInputData,
+            map: map,
+            reduce: reduce,
+            summarize: summarize
+        };
+    }
+);
