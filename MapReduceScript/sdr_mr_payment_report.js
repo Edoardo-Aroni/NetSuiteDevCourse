bded90533e9f85618d63202a3203b740edaa499d
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-09-01 12:55:37 +0100

    s

--- MapReduceScript/sdr_mr_payment_report.js
+++ MapReduceScript/sdr_mr_payment_report.js
@@ -27,22 +27,48 @@ define(['N/search'],
             log.debug('Resulting Value:', searchResult.values);
 
             var customer = searchResult.values.entity.text;
-            var status = searchResult.values.statusref.text;
+            var status = searchResult.values.statusref;
             var amount = searchResults.values.amountpaid;
 
             context.write({
                 key: customer,
-                value: status && amount
+                value: {
+                    status: status,
+                    amount: amount
+                }
             });
 
         }
         function reduce(context){
+            var depositedTotal = 0;
+            var unDepositedTotal = 0;
+
+            for ( var i in context.values){
+                var value = JSON.parse(context.values[i]);
+                if(value.status == 'deposited'){
+                    depositedTotal += parseFloat(value.amount);
+                }
+                if(value.status == 'undeposited'){
+                    unDepositedTotal += parseFloat(value.amount);
+                }
+            }
+
+            log.debug(context.key, 'Deposited Total: ' + depositedTotal + '\n' +
+                                   'Undeposited Total: ' + unDepositedTotal);
+        }
+        function summarize(summary){
+            
+            log.audit('Usage Consumed', summary.usage);
+            log.audit('Number of Queues used', summary.concurrency);
+            log.audit('Number of Yields done', summary.yields);
+
 
         }
         return {
             getInputData: getInputData,
             map: map,
-            reduce: reduce
+            reduce: reduce,
+            summarize: summarize
         }
     }
 );
