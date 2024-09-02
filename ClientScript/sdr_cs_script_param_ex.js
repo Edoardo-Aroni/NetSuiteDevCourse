f8c8a11f42e8b92d134736c8b237fc453c0d447e
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-09-02 06:53:49 +0100

    s

---
+++ ClientScript/sdr_cs_script_param_ex.js
@@ -0,0 +1,32 @@
+/**
+ * @NApiVersion 2.0
+ * @NScriptType ClientScript
+ */
+
+define(['N/runtime'],
+    /**
+     * 
+     * @param {runtime} runtime 
+     * @returns 
+     */
+    function(runtime){
+        return {
+            saveRecord: function saveRecord(context){
+               var script = runtime.getCurrentScript();
+               var recordType = script.getParameter({
+                name: 'custscript_sdr_save_record_type'
+               });
+               var confirmation = confirm('Click OK if are sure you would like to confirm your change' + '\n' +
+                                          'for this ' + recordType + '. Click Cancel to continue editing.');
+               if(confirmation) {
+                return true;
+               } else {
+                return false;
+               }
+               
+
+            }
+        }
+        
+    }
+);
