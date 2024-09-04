beea18b0c325cb9eb24333b616bb4ddc58a4bc08
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-09-04 10:07:29 +0100

    s

---
+++ WorkflowActionScript/sdr_wf_update_employee.js
@@ -0,0 +1,40 @@
+/**
+ * @NApiVersion 2.0
+ * @NScriptType workflowactionscript
+ */
+
+define(['N/record','N/runtime'],
+/**
+ * 
+ * @param {record} record 
+ * @param {runtime} runtime 
+ * @returns 
+ */
+    function(record,runtime){
+        function onAction(context){
+            var workflowTotal = runtime.getCurrentScript().getParameter({
+                name:'custscript_sdr_workflow_total'
+            });
+
+            var expRep = context.newRecord;
+            var expenseCount = expRep.getLineCount({
+                sublistId: 'expense'
+            });
+            var employeeId = expRep.getValue('entity');
+            var notes = 'Workflow Total: ' + workflowTotal + '\n' +
+                        'Expense Count: ' + expenseCount;
+
+            var employee = record.load({
+                type: record.Type.EMPLOYEE,
+                id: employeeId
+            });
+
+            employee.setValue('comment','notes');
+            employee.save();
+
+        }
+        return {
+            onAction: onAction
+        };
+    }
+);
