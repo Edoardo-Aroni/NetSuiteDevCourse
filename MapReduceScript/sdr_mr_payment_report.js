184ddd41a6dacdca5282f01cb5569bdec6b76913
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-09-05 07:07:36 +0100

    s

--- MapReduceScript/sdr_mr_payment_report.js
+++ MapReduceScript/sdr_mr_payment_report.js
@@ -3,13 +3,13 @@
  * @NScriptType MapReduceScript
  */
 
-define(['N/search'], 
+define(['N/search', 'N/runtime'], 
     /**
      * 
      * @param {search} search 
      * @returns 
      */
-    function(search){
+    function(search, runtime){
 
         function getInputData(){
         //     var paymtSearch = search.load({
@@ -17,9 +17,33 @@ define(['N/search'],
         //     });
         //     return paymtSearch;
 
-            return search.load({
-                id: 'customsearch_sdr_payments'
-            });
+            // return search.load({
+            //     id: 'customsearch_sdr_payments'
+            // });
+        
+        // Get the customer id from the script parameter passed by the task 
+        // set in the user event script
+
+        var script = runtime.getCurrentScript();
+        var customerId = script.getParameter({
+            name: 'custscript_sdr_customer_id'
+        });
+
+        // create the saved search for payments and add the customer id
+        // parameter as a filter
+
+        var paymentSearch = search.create({
+            type: search.Type.TRANSACTION,  
+            filters:[
+                ['type', search.Operator.ANYOF, 'CustPymt'], 'and'
+                ['mainline', search.Operator.IS, true],'and',
+                ['entity',search.Operator.ANYOF, customerId]
+            ],
+            columns:['entity', 'statusref', 'amounpaid']
+        });
+
+        return paymentSearch;
+        
         }
 
         function map(context){
