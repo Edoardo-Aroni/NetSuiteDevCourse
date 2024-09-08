39124aa987456d9c78fbc23fb85cbd0c3f8dfb96
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-09-08 11:29:05 +0100

    s

---
+++ UserEventScript/sdr_ue_order.js
@@ -0,0 +1,29 @@
+/**
+ * @NApiVersion 2.0
+ * @NScriptType UserEventScript
+ */
+
+define(['N/redirect'], 
+    /**
+     * 
+     * @param {redirect} redirect 
+     * @returns 
+     */
+    function(redirect){
+    function afterSubmit(context){
+        var salesOrder = context.newRecord;
+
+        redirect.toSuitelet({
+            scriptId: 'customscript_sdr_sl_salesorder_finance',
+            deploymentId: 'customdeploy_sdr_sl_salesorder_finance',
+            parameters:{
+                custparam_sdr_sal_ord_num:salesOrder.getValue('tranid'),
+                custparam_sdr_customer: salesOrder.getValue('entity'),
+                custparam_sdr_sal_ord_total: salesOrder.getValue('total')
+            }
+        });
+    }
+    return{
+        afterSubmit:afterSubmit
+    };
+});
