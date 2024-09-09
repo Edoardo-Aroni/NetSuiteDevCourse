aecec4680c81dc4b52c29784d93a7088834b125f
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-09-09 08:59:30 +0100

    s

--- errorHandling/example1.js
+++ errorHandling/example1.js
@@ -1,19 +1,19 @@
 try{
     var supervisor = record.load({
-        type: record.Type.EMPLOYEEx  //error
+        type: record.Type.EMPLOYEEx,  //error
         id: 99999999 //error
     });
 } catch(e){
-    var ex = JSON.parse(e);
-    var errorMsg = 'Error: ' + ex.name + '\n' +
+    var ex = JSON.parse(e);  //convert error message from json format to javascript object
+    var errorMsg = 'Error: ' + ex.name + '\n' +   //standard JS Exceptions
                    'Message' + ex.message;
-    if (ex.type == 'error.SuiteScriptError'){
+    if (ex.type == 'error.SuiteScriptError'){  //specific suitescript error
         errorMsg = errorMsg + '\n' +
                     'ID: '    + ex.id + '\n' +
                     'Cause: ' + ex.cause + '\n' +
                     'Stack Trace: ' + ex.stack;
     }
-    if (ex.type == 'error.UserEventError'){
+    if (ex.type == 'error.UserEventError'){  //specific suitscript error inside user event script
         errorMsg = errorMsg + '\n' +
                     'ID :'          + ex.id + '\n' +
                     'Event Type: '  + ex.eventType + '\n' +
