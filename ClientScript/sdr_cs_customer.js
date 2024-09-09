230d513150c45ca4ef9b9029d2287480236f7da8
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-09-09 22:40:38 +0100

    s

--- ClientScript/sdr_cs_customer.js
+++ ClientScript/sdr_cs_customer.js
@@ -3,7 +3,13 @@
  * @NApiVersion 2.0
  */
 
-define([], function() {
+define(['N/https','N/url'], 
+    /**
+     * @param {https} https 
+     * @param {url} url 
+     */
+    
+    function(https, url) {
 
     function pageInit(context){
         var customer = context.currentRecord;
@@ -77,39 +83,53 @@ define([], function() {
         var customer = context.currentRecord;
         var applyCoupon = customer.getValue('custentity_sdr_apply_coupon');
         var couponCode = customer.getValue('custentity_sdr_coupon_code');
-        if(applyCoupon && couponCode.length != 5 ){
-            alert('The coupon code length is not 5 characters. Please try again.');
+        // if(applyCoupon && couponCode.length != 5 ){
+        //     alert('The coupon code length is not 5 characters. Please try again.');
+        //     return false;
+        // }
+
+        var restletUrl= url.resolveScript({
+            scriptId:'customscript_sdr_rl_coupon_code',
+            deploymentId: 'customdeploy_sdr_rl_coupon_code'
+        });
+
+        var response = https.get({
+            url:restletUrl + '&custparam_couponcode='+ couponCode //internal url
+        });
+
+        if (applyCoupon && response.body == 'invalid') {
+            alert('The coupon code is not valid. Please try again.');
             return false;
         }
 
-        var prodPrefCount = customer.getLineCount({
-            sublistId: 'recmachcustrecord_sdr_prod_pref_customer'
-        });
+        // var prodPrefCount = customer.getLineCount({
+        //     sublistId: 'recmachcustrecord_sdr_prod_pref_customer'
+        // });
 
-        var totPrefQty = 0;
+        // var totPrefQty = 0;
 
-        for(var i = 0; i < prodPrefCount; i++){
-            var prefQty = customer.getSublistValue({
-                sublistId: 'recmachcustrecord_sdr_prod_pref_customer',
-                fieldId: 'custrecord_sdr_prod_pref_qty',
-                line: i
-            });
-            totPrefQty += parseInt(prefQty);
+        // for(var i = 0; i < prodPrefCount; i++){
+        //     var prefQty = customer.getSublistValue({
+        //         sublistId: 'recmachcustrecord_sdr_prod_pref_customer',
+        //         fieldId: 'custrecord_sdr_prod_pref_qty',
+        //         line: i
+        //     });
+        //     totPrefQty += parseInt(prefQty);
 
-            if(totPrefQty > 25 ){
-                alert('The total preferred quantity across all product preferences has exceed the limit of 25.');
-                return false;
-            } 
-        }
+        //     if(totPrefQty > 25 ){
+        //         alert('The total preferred quantity across all product preferences has exceed the limit of 25.');
+        //         return false;
+        //     } 
+        // }
         return true;
     }
 
     return {
-        pageInit:       pageInit,
-        lineInit:       lineInit,
-        validateLine:   validateLine,
-        fieldChanged:   fieldChanged,
-        validateField:  validateField,
+        // pageInit:       pageInit,
+        // lineInit:       lineInit,
+        // validateLine:   validateLine,
+        // fieldChanged:   fieldChanged,
+        // validateField:  validateField,
         saveRecord:     saveRecord
     };
 });
