b2e2cf8f28952bc039f0b4baf98b086f581a31d7
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-09-20 20:13:14 +0100

    s

---
+++ N Query/snippets_output_types.js
@@ -0,0 +1,75 @@
+//sample
+
+var results = myCreatedQuery.runPage({
+    pageSize: 10
+});
+log.debug({
+    title: 'Total Pages: ',
+    details: results.PageRanges.length
+});
+log.debug({
+    title:'Total Result Size: ',
+    details: results.count
+});
+
+var myPageResults = results.iterator();
+
+myPageResults.each(function(page){
+    log.debug('Page Start');
+    var pageIterator = page.value.data.iterator();
+    pageIterator.each(function(row){
+        log.debug({
+            title:'Entity ID: ',
+            details: row.value.getValue(0)
+        });
+        return true;
+    })
+    return true;
+})
+
+// sample asMap()
+
+myCreatedQuery.columns = [
+    myCreatedQuery.createColumn({
+        fieldId: 'entityid',
+        alias: 'customer'
+    }),
+    myCreatedQuery.createColumn({
+        fieldId: 'salesrep',
+    }),
+    myCreatedQuery.createColumn({
+        fieldId: 'overduebalancesearch',
+    }),   
+];
+
+var resultSet = myCreatedQuery.run();
+var results = resultSet.results;
+
+for(var i in results){
+    log.debug({
+        title:results[i].asMap()
+    });
+}
+
+// sample asMappedResults()
+
+myCreatedQuery.columns = [
+    myCreatedQuery.createColumn({
+        fieldId: 'entityid',
+        alias: 'customer'
+    }),
+    myCreatedQuery.createColumn({
+        fieldId: 'salesrep',
+    }),
+    myCreatedQuery.createColumn({
+        fieldId: 'overduebalancesearch',
+    }),   
+];
+
+var resultSet = myCreatedQuery.run();
+var results = resultSet.asMappedResults();
+
+log.debug({
+    title: results
+});
+
