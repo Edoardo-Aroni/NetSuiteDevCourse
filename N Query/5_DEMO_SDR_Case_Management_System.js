7f8491e13aa54d38f4bbf007ef9f36d17c4f115b
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-09-21 06:25:16 +0100

    s

--- N Query/5_DEMO_SDR_Case_Management_System.js
+++ N Query/5_DEMO_SDR_Case_Management_System.js
@@ -1,49 +1,108 @@
-requestAnimationFrame(['N/query'],
-    function(query){
+require(['N/query'],
+    function(query) {
         var myLoadedQuery = query.load({
-            id:'custworkbook_sdr_wb_employee_cases'
+            id: 'custworkbook_sdr_wb_employee_cases'
         });
 
         var customerJoin = myLoadedQuery.joinTo({
-            fieldId:'company',
-            target:'Customer'
+            fieldId: 'company',
+            target: 'Customer'
         });
 
         var contactJoin = customerJoin.autoJoin({
-            fieldId:'contactList'
+            fieldId: 'contactList'
         });
 
         var firstCondition = myLoadedQuery.createCondition({
-            fieldId:'category',
-            operator:query.Operator.ANY_OF,
-            values:[1,2,3]
+            fieldId: 'category',
+            operator: query.Operator.ANY_OF,
+            values: [1, 2, 3]
         });
 
         var secondCondition = myLoadedQuery.createCondition({
-            fieldId:'timeelapsed',
-            operator:query.Operator.GREATER,
-            values:0
+            fieldId: 'timeElapsed',
+            operator: query.Operator.GREATER,
+            values: 0
         });
 
         var thirdCondition = contactJoin.createCondition({
-            fieldId:'phone',
-            operator:query.Operator.EMPTY_NOT
+            fieldId: 'phone',
+            operator: query.Operator.EMPTY_NOT
         });
 
-        myLoadedQuery.condition = myLoadedQuery.and(firstCondition,secondCondition,thirdCondition);
+        myLoadedQuery.condition = myLoadedQuery.and(firstCondition, secondCondition, thirdCondition);
 
         myLoadedQuery.columns = [
+            myLoadedQuery.createColumn({
+                fieldId: 'caseNumber',
+                alias: 'Case Number'
+            }),
+            myLoadedQuery.createColumn({
+                fieldId: 'startDate',
+                alias: 'Incident Date'
+            }),
+            myLoadedQuery.createColumn({
+                fieldId: 'title',
+                alias: 'Subject'
+            }),
+            myLoadedQuery.createColumn({
+                fieldId: 'status',
+                context: query.FieldContext.DISPLAY,
+                alias: 'Status'
+            }),
+            myLoadedQuery.createColumn({
+                fieldId: 'assigned',
+                context: query.FieldContext.DISPLAY,
+                alias: 'Assigned'
+            }),
+            myLoadedQuery.createColumn({
+                fieldId: 'company',
+                context: query.FieldContext.DISPLAY,
+                alias: 'Company'
+            }),
+            myLoadedQuery.createColumn({
+                fieldId: 'escalateTo',
+                context: query.FieldContext.DISPLAY,
+                alias: 'Escalated To'
+            }),
+            myLoadedQuery.createColumn({
+                fieldId: 'lastCustomerMessageReceived',
+                alias: 'Last Message Received'
+            }),
+            contactJoin.createColumn({
+                fieldId: 'phone',
+                alias: 'Customer Contact'
+            }),
+            myLoadedQuery.createColumn({
+                type: query.ReturnType.STRING,
+                formula: `CONCAT('Days: ', TO_CHAR(FLOOR({timeElapsed}/24)),
+                                 '| hours: ', 
+                                 TO_CHAR(MOD(TO_NUMBER({timeElapsed}),24)) )`,
+                alias: 'Days | Hours'
+            }),
+            myLoadedQuery.createColumn({
+                type: query.ReturnType.STRING,
+                formula: `CASE
+                          WHEN ({timeElapsed}/24)*20 > 0
+                          THEN TO_CHAR(({timeElapsed}/24)*20, '99,999,999.99')
+                          ELSE '0.00'
+                          END`,
+                alias: 'Billable Amount'
+            })
+        ];
+
+        var resultSet = myLoadedQuery.run();
+        var results = resultSet.results;
 
 
-        ]
+        log.debug({
+            title: 'Query Length',
+            details: results.length
+        });
 
-        //8:31
+        for(var i=0; i<results.length; i++){
+            var mResult = results[i].values;
 
-
-
-
-
-
-
-
+            log.debug({title: mResult});
+        }    
 });
