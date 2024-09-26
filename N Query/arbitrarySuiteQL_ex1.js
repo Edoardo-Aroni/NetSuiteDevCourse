d50c20c4971896bf56cfff1ce41a1cdc1767cf93
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-09-26 06:36:18 +0100

    s

--- N Query/arbitrarySuiteQL_ex1.js
+++ N Query/arbitrarySuiteQL_ex1.js
@@ -1,17 +1,36 @@
 require(['N/query'],
     function(query){
         var invcsale = `SELECT
-                          transaction.closedate,
-                          transaction.foreigntotal
+                          transaction.closedate AS 'date closed',
+                          transaction.foreigntotal AS 'fc total amount' ,
+                          BUILTIN.DF(customer.id) AS 'customer name',
+                          BUILTIN.DF(customer.salesrep) AS 'sales rep'
                         FROM
-                          transaction,
-                          transactionline
-                        WHERE
+                          transaction
+                          INNER JOIN transactionline ON
                           transaction.id = transactionline.transaction
-                          AND transaction.type = 'CustInvc'
+                          INNER JOIN customer ON
+                          transactionLine.entity = customer.id
+                        WHERE
+                          transaction.type = 'CustInvc'
                           AND transaction.status = 'CustInvc:B'
                           AND transaction.closedate BETWEEN '01/01/2024' AND '31/03/2024'
-                          AND transactionline.mainline IS true`;
-        
+                          AND transactionline.mainline = 'T'
+                          AND customer.salesrep = -5`;
+     // execute query                     
+     var resultSet = query.runSuiteQL({
+      query: invcsale
+     });
+     // fetch query results
+     var results = resultSet.results;
+     // display results length
+     log.debug({
+      title:'Query Length: ',
+      details: results.length
+     });
+     //iterate through the results
+     for(var i=0; i<results.length; i++){
+      log.debug(results[i].values);
+     }
 
-})
+});
