479cdd82c79d8fe1fff9b9776653343f29a1cb18
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-09-27 15:26:34 +0100

    s

--- Suitelet/sdr_sl_performance_review_custom.js
+++ Suitelet/sdr_sl_performance_review_custom.js
@@ -14,7 +14,7 @@ define(['N/ui/serverWidget','N/query','N/url'],
         var emp_disp = request.parameters.custparam_employee_disp;
 
         var myLoadedQuery = query.load({
-            id: 'custworkbook_sdr_case'
+            id: 'custworkbook_sdr_wb_employee_cases'
         });
 
         var customerJoin = myLoadedQuery.joinTo({
@@ -22,7 +22,7 @@ define(['N/ui/serverWidget','N/query','N/url'],
             target: 'customer'
         });
 
-        var campaignJoin = customerJoin.joinTo({
+        var leadSourceJoin = customerJoin.joinTo({
             fieldId:'leadsource',
             target:'searchcampaign'
         });
@@ -42,8 +42,14 @@ define(['N/ui/serverWidget','N/query','N/url'],
         myLoadedQuery.condition = myLoadedQuery.and(firstCondition, secondCondition);
             
         myLoadedQuery.columns = [
-            campaignJoin.createColumn({
-                fieldId:'category'
+            myLoadedQuery.createColumn({
+                fieldId:'casenumber'
+            }),
+            myLoadedQuery.createColumn({
+                fieldId:'startdate'
+            }),
+            myLoadedQuery.createColumn({
+                fieldId:'title'
             }),
             myLoadedQuery.createColumn({
                 fieldId:'status',
@@ -59,6 +65,10 @@ define(['N/ui/serverWidget','N/query','N/url'],
             }),
             myLoadedQuery.createColumn({
                 fieldId:'escaleteto',
+                context:query.FieldContext.DISPLAY
+            }),
+            myLoadedQuery.createColumn({
+                fieldId:'category',
                 context:query.FieldContext.DISPLAY
             }),
             myLoadedQuery.createColumn({
@@ -84,33 +94,72 @@ define(['N/ui/serverWidget','N/query','N/url'],
             details: results.length
         });
 
+        var list = serverWidget.createList({
+            title:'Cases Handled by: ' + emp_disp
+        });
+
+        list.addColumn({
+            id:'casenumber',
+            type: serverWidget.FieldType.TEXT,
+            label: 'Case Number'
+        });
+        list.addColumn({
+            id:'startdate',
+            type: serverWidget.FieldType.TEXT,
+            label: 'Start Date'
+        });
+        list.addColumn({
+            id:'title',
+            type: serverWidget.FieldType.TEXT,
+            label: 'Subject'
+        });
+        list.addColumn({
+            id:'status',
+            type: serverWidget.FieldType.TEXT,
+            label: 'Status'
+        });
+        list.addColumn({
+            id:'assigned',
+            type: serverWidget.FieldType.TEXT,
+            label: 'Assigned To'
+        });
+        list.addColumn({
+            id:'company',
+            type: serverWidget.FieldType.TEXT,
+            label: 'Company'
+        });
+        list.addColumn({
+            id:'escalateto',
+            type: serverWidget.FieldType.TEXT,
+            label: 'Escalate To'
+        });
+        list.addColumn({
+            id:'lscampaign',
+            type: serverWidget.FieldType.TEXT,
+            label: 'Lead Source Campaign Category'
+        });
+        list.addColumn({
+            id:'dayshours',
+            type: serverWidget.FieldType.TEXT,
+            label: 'Day | Hours'
+        });
         
+        for(var i in results){
+            list.addRows({
+                rows:[{
+                    casenumber: results[i].values[0] + '',
+                    startdate: results[i].values[1] + '',
+                    title: results[i].values[2] + '',
+                    status: results[i].values[3] + '',
+                    assigned: results[i].values[4] + '',
+                    company: results[i].values[5] + '',
+                    escalateto: results[i].values[6] + '',
+                    lscampaign: results[i].values[7] + '',
+                    dayhours: results[i].values[8] + ''}]
+            });
+        }
 
-
-
-
-
-
-
-
-     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        response.writePage(list);
 
     }
     return {
