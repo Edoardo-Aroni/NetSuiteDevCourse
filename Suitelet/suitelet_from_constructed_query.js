d3a9b38e1599f3395d2c00ca0afcd2dc1bb0c6c7
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-09-28 07:29:51 +0100

    s

--- Suitelet/suitelet_from_constructed_query.js
+++ Suitelet/suitelet_from_constructed_query.js
@@ -43,7 +43,7 @@ define(['N/query','N/redirect','N/ui/serverWidget'],
 
             log.debug(dept_name);
 
-            // Redirect to another Suitelet, passing the selected date as a parameter
+            // Redirect to another Suitelet, passing paremeters
             redirect.toSuitelet({
                 scriptId: 'customscript_sdr_suitelet_query2', // Replace with your second Suitelet script ID
                 deploymentId: 'customdeploy_sdr_suitelet_query2', // Replace with your second Suitelet deployment ID
