e17e99b31f293a05aaa40842f6b9bb74286319a4
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-10-05 17:21:25 +0100

    s

---
+++ TEMPLATE 2.1/suitelet.js
@@ -0,0 +1,19 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ * @NModuleScope SameAccount
+ */
+define([],
+
+    () => {
+       
+
+        const onRequest = (context) => {
+    
+        }
+    
+        return {
+            onRequest: onRequest
+        };
+        
+    });
