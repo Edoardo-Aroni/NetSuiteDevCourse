6f272f097680a02ca63b476fc80ad02822487e50
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-10-08 16:32:08 +0100

    s

--- N Query/eii_sweep_query.js
+++ N Query/eii_sweep_query.js
@@ -5,6 +5,7 @@ require(['N/query'],
         const sweepQuery = query.create({
             type: query.Type.TRANSACTION
         });
+
         //JOINS
         // join to Transaction Line
         const transactionLineJoin = sweepQuery.autoJoin({fieldId:'transactionlines'});
@@ -12,10 +13,18 @@ require(['N/query'],
         const subsidiaryJoin = transactionLineJoin.autoJoin({fieldId:'subsidiary'});
         // join to Class
         const classJoin = transactionLineJoin.autoJoin({fieldId:'class'});
+        // join to Product
+        const productJoin = transactionLineJoin.autoJoin({fieldId:'custcol_cseg_eii_product_id'});
+        // join to Department = Functional Activity
+        const departmentJoin = transactionLineJoin.autoJoin({fieldId:'department'});
+        // join to Location
+        const locationJoin = transactionLineJoin.autoJoin({fieldId:'location'});
+
         // join to Transaction Audit Line
         const transactionAccountingLineJoin = transactionLineJoin.autoJoin({fieldId:'accountingimpact'});
         // join to Account
         const accountJoin = transactionAccountingLineJoin.autoJoin({fieldId:'account'});
+
         // CONDITIONS
         //posting is true
         const oneCondition = sweepQuery.createCondition({
@@ -23,19 +32,19 @@ require(['N/query'],
             operator: query.Operator.IS,
             values: true
         });
-        //posting is within this period
+        //posting is within this period - Oct 2024
         const twoCondition = sweepQuery.createCondition({
             fieldId: 'postingperiod',
             operator: query.Operator.ANY_OF,
             values: [443]
         });
-        //subsidiary is 21
+        //subsidiary is 21 = EMT
         const threeCondition = transactionLineJoin.createCondition({
             fieldId: 'subsidiary',
             operator: query.Operator.ANY_OF,
             values:[21]
         });
-        //class is
+        //class is MBC = 258
         const fourCondition = transactionLineJoin.createCondition({
             fieldId: 'class',
             operator: query.Operator.ANY_OF,
@@ -80,12 +89,16 @@ require(['N/query'],
         */
        // COLUMNS
        sweepQuery.columns = [
-        sweepQuery.createColumn({fieldId: 'postingperiod.enddate', alias:'trans_date'}),
-        subsidiaryJoin.createColumn({fieldId: 'currency', context: query.FieldContext.DISPLAY}),
-        accountJoin.createColumn({fieldId: 'acctnumber'}),
-        transactionAccountingLineJoin.createColumn({fieldId:'debit'}),
-        transactionAccountingLineJoin.createColumn({fieldId:'credit'}),
-        classJoin.createColumn({fieldId: 'externalid'})
+        sweepQuery.createColumn({fieldId:'postingperiod.enddate'}),                             //trans_date
+        subsidiaryJoin.createColumn({fieldId:'currency', context: query.FieldContext.DISPLAY}), //currency
+        accountJoin.createColumn({fieldId:'acctnumber'}),                                       //account
+        transactionAccountingLineJoin.createColumn({fieldId:'debit'}),                          //debit
+        transactionAccountingLineJoin.createColumn({fieldId:'credit'}),                         //credit
+        classJoin.createColumn({fieldId:'externalid'}),                                         //class
+        productJoin.createColumn({fieldId:'externalid'}),                                       //product_id
+        departmentJoin.createColumn({fieldId:'externalid'}),                                    //functional_activity
+        locationJoin.createColumn({fieldId:'externalid'}),                                      //location_code
+        transactionLineJoin.createColumn({fieldId:'custcol_eii_jnl_country', context:query.FieldContext.DISPLAY}) // country                                  //country
        ];
 
 
