1075f8aaa5ad327a50b4b63fb9c4f821d1e0da4e
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-10-09 21:20:56 +0100

    s

---
+++ TEMPLATE 2.1/portlet script.js
@@ -0,0 +1,20 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Portlet
+ * @NModuleScope SameAccount
+ */
+define([],
+
+    () => {
+       
+        // Definition of the Portlet script trigger point.
+        const render = (params) => {
+    
+        }
+    
+        return {
+            render: render
+        };
+        
+    });
+    
