fa94d613d960f2e8878de855a991fed3c65311e9
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-10-09 21:19:01 +0100

    s

---
+++ TEMPLATE 2.1/scheduled script.js
@@ -0,0 +1,20 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ * @NModuleScope SameAccount
+ */
+define([],
+
+    () => {
+       
+        // Definition of the Suitelet script trigger point.
+
+        const onRequest = (context) => {
+    
+        }
+    
+        return {
+            onRequest: onRequest
+        };
+        
+    });
