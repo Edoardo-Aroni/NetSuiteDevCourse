c0ed41216567b61d908c83c589369a95d7e3086b
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-10-21 07:03:20 +0100

    s

--- A_E/clientScripts/cs_phone_numb_message.js
+++ A_E/clientScripts/cs_phone_numb_message.js
@@ -2,6 +2,7 @@
  * Business scope:
  * Validate if phone number is filled out when user submits the customer record.
  * If empty, show an error message and prevent the form submission.
+ * Get message title and text from script parameters
  *
  * Date                 Author                      Comments
  * 18 Oct 2024          Edo Aroni                   N/A
@@ -9,27 +10,33 @@
  * @NApiVersion 2.1
  * @NScriptType ClientScript
  */
-define(['N/ui/message'], function(message) {
+define(['N/ui/message', 'N/runtime'], function(message, runtime) {
     const saveRecord = (context) => {
-        const customer = context.currentRecord;
-        const phoneNumb = customer.getValue({ fieldId: 'phone' });    
+
+        const customer  = context.currentRecord;
+        const phoneNumb = customer.getValue({ fieldId: 'phone' }); 
+
+        // Get script parameters for error message title and text
+        const msgTitle  = runtime.getCurrentScript().getParameter({name: 'custscript_msg_title'});  
+        const msgText   = runtime.getCurrentScript().getParameter({name: 'custscript_msg_txt'});
         
         if (!phoneNumb) {
             let errMsg = message.create({
-                title: 'Missing phone number',
-                message: 'Please enter the phone number before saving.',
+                title: msgTitle || 'set custom title', // Fallback if msgTitle is null
+                message: msgText || 'set custom message', // Fallback if msgText is null
                 type: message.Type.ERROR
             });
+
             errMsg.show();  // Show error message
             return false;   // Prevent form submission
         }
 
         // If phone number exists, allow the form to submit
         return true;
+       
     }
-
     return {
-        saveRecord: saveRecord  // Return the correct function
+        saveRecord  // Return the correct function
     };
 });
 
