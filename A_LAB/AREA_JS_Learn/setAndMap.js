c4ac4dce85616f6c86fbc454234609eff2bacad7
Edoardo-Aroni <edoardoaroni@gmail.com>
2024-10-22 06:11:29 +0100

    s

--- A_LAB/AREA_JS_Learn/setAndMap.js
+++ A_LAB/AREA_JS_Learn/setAndMap.js
@@ -9,10 +9,6 @@ Maps are like object, exceptions:
 - map preserve the order of keys
   - for loops are predictable. They come back always in the same order
 - must be accessed and changed using special methods
-
-
-
-
 */
 
 // create using constructors
@@ -43,4 +39,64 @@ mySet.add(2);
 mySet.add(3);
 mySet.add(5);
 mySet.add(8);
+mySet.add(13);
+mySet.add('fibonacci');
+
+// how transform an array into a set and remove duplicates
+// by passing an array to the set constructor
+let mySet2 = new Set(myList);
+//to check if an item is in the set
+mySet2.has(5); // return true
+
+//how to loop through the items in the set
+for(let item of mySet2) {
+  console.log('myset contains', item);
+}
+
+// comparing Map to Object
+
+//this object is a bird
+var bird = {
+  genus: 'corvus',
+  species: 'corvax',
+  commonName:'raven',
+}
+
+//to create a Map
+var birdMap = new Map();
+birdMap.set('genus','corvus'); // the set method has the Key, Value pairs
+birdMap.set('species','corvax');
+birdMap.set('commonName','raven');
+
+//to get a value by his key use the get method
+birdMap.get('genus'); //corvus
+
+//Map has also an has method to check if a key exists but it does now work for values
+birdMap.has('species'); //true
+birdMap.has('corvax'); // false (it works only for keys)
+
+// for loop work on Maps,  with key and value returned
+for(let value of birdMap) {
+  console.log(value);
+} // it returns a series of arrays with key and values
+
+// using the entries method we get a similar output but with different format
+birdMap.entries();
+
+// how to convert an objet to a Map
+Object.entries(bird); // return an array of arrays
+
+//to create a new bird Map from an object
+birdMap2 = new Map(Object.entries(bird));
+
+
+
+
+
+
+
+
+
+
+
 
